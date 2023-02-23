c3bb5be1a8a4d82f6b069b5311ba8c9b9464b860
SteveDelFante <sdelfante@chef.io>
2023-02-23 10:50:57 -0800

    3rd time

---
+++ custom_web/recipes/default.rb
@@ -0,0 +1,32 @@
+#
+# Cookbook:: custom_web
+# Recipe:: default
+#
+# Copyright:: 2023, The Authors, All Rights Reserved.
+apt_update 'update apt database'
+
+package 'apache2'
+
+file '/var/www/html/index.html' do
+  content '<h1>Welcome home!</h1>'
+end
+
+directory '/srv/apache/admins/html' do
+  recursive true
+  mode '0755'
+end
+
+file '/srv/apache/admins/html/index.html' do
+  content '<h1>Welcome admins!</h1>'
+end
+
+template '/etc/apache2/sites-enabled/admins.conf' do
+  source 'conf.erb'
+  mode '0644'
+  variables(document_root: '/srv/apache/admins/html', port: 8080)
+  notifies :reload, 'service[apache2]'
+end
+
+service 'apache2' do
+  action [:enable, :start]
+end
